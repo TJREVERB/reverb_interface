11d365120fb9d27bad192d2f42acfbac32a33a06
Alan-JH <alanjhsu08@gmail.com>
2021-11-09 15:57:30 -0500

    I have no idea what Im doing with this firmware

--- REVERB Interface Multicore/Attinyslave/Attinyslave.ino
+++ REVERB Interface Multicore/Attinyslave/Attinyslave.ino
@@ -1,31 +1,95 @@
 #include <SPI.h>
 #include <Wire.h>
-#include "pins_arduino.h"
 
 // SPI pin definitions
 #define MOSI 8    // MOSI / PA1
 #define MISO 9    // MISO / PA2
-#define SCK  10    // SCK  / PA3
+#define SCK  10   // SCK  / PA3
 #define CS   0    // SSb  / PA4
 
-byte buf [32];
+#define SPIIN PINA
+#define SPIOUT PORTA
+#define SPIDIR DDRA
+#define MOSIPORT 1
+#define MISOPORT 2
+#define SCKPORT 3
+#define CSPORT 4
+
+byte mode = 1; //device mode: 1 uart repeater, 2 i2c delay. default to uart repeater, switch modes upon recieving specified byte:
+byte switchmode = 0x0a; //\n
+volatile byte inbuf [100]; //input buffer
+volatile byte outbuf [100]; //output buffer
+//volatile byte inbuf; //input buffer byte
+//volatile byte outbuf = 255; //output buffer byte
 volatile byte pos;
-volatile boolean process_it;
+volatile byte bytepos
+boolean reading = 0;
 
 void setup() {
-  pinMode(MISO, OUTPUT); //Set up SPI slave
+  pinMode(MISO, INPUT); //Set up SPI slave
   pinMode(MOSI, INPUT);
   pinMode(SCK,  INPUT);
-  pinMode(SS,   INPUT);
-  SPCR |= _BV(SPE);
-  SPCR |= _BV(SPIE);
-
+  pinMode(SS,   INPUT_PULLUP);
+  
   pos = 0;
   Serial.begin(19200); //Begin uart
-
+  
   Wire.begin(); //Begin i2c as master
 }
 
-void loop() {
+void readData(){
+  inbuf[bytepos] |= ((SPIIN >> MOSIPORT) & 1) << pos; //set next input bit
+  //SPIOUT |= ((outbuf[bytepos] >> pos) & 1 << MISOPORT); //set output to next output bit
+  //SPIOUT &= ~((outbuf[bytepos] >> pos) & 1 << MISOPORT);
+  pos++;
+}
 
+void loop() {
+  if (digitalRead(SS) == LOW && !reading){ //If slave pin pulled low, start read. Recommend 1ms delay on master device after SS pulled low
+    pos = 0;
+    reading = 1;
+    SPIDIR |= (1 << MISOPORT);
+    SPIOUT |= (1 << MISOPORT);
+    attachInterrupt(digitalPinToInterrupt(SCK), readData, RISING); //RISING EDGE, MODE0
+  }
+  if (digitalRead(SS) == HIGH && reading) { //If slave pin is pulled high after being low, end read.
+    detachInterrupt(digitalPinToInterrupt(SCK));
+    SPIDIR &= ~(1 << MISOPORT);
+    reading = 0;
+  }
+  if (pos > 7){ //move on to the next byte
+    bytepos++;
+  }
+  
+
+  if (!reading){
+    if (inbuf[0] == switchbyte){ //Switch modes: buffer is assumed to be two bytes long, first byte switchbyte, second byte mode
+      mode = inbuf[1];
+      bytepos = 0;
+    } else if (mode == 1){ //UART repeater mode: sends all bytes received from spi over serial, reads result and returns
+      for (int i = 0; i < bytepos + 1; i++){
+        Serial.write(inbuf[i]);
+      }
+      bytepos = 0;
+
+      int txln = Serial.available();
+      for (int i = 0; i < txln; i++){
+        outbuf[i] = Serial.read();
+      }
+    } else if (mode == 2){ //I2C repeater mode: syntax: 1st byte delay in increments of 0.01s, 2nd byte device address, 3rd byte and beyond data, reads result and returns
+      delay(10*inbuf[0]);
+      Wire.beginTransmission(inbuf[1]);
+      for (int i = 2; i < bytepos + 1; i++){
+        Wire.write(inbuf[i]);
+      }
+      Wire.endTransmission();
+      byteos = 0;
+
+      int txln = 0;
+      while (Wire.available()){
+        outbuf[i] = Wire.read();
+        txln ++;
+      }
+    }
+  }
 }
