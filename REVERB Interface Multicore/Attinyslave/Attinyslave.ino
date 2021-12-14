11bf9c1297cee2e6f4cead238451c2c699761955
Alan-JH <alanjhsu08@gmail.com>
2021-12-14 09:33:54 -0500

    Update Attinyslave.ino

--- REVERB Interface Multicore/Attinyslave/Attinyslave.ino
+++ REVERB Interface Multicore/Attinyslave/Attinyslave.ino
@@ -13,29 +13,20 @@ void setup() {
 void receiveEvent(int len){
   byte mode = Wire.read();
   if (mode == 0){ //UART transmit
-    for (int i = 1; i < len; i++){
-      byte b = Wire.read();
-      Serial.write(b);
-    }
+    byte b = Wire.read();
+    Serial.write(b);
   }else if (mode == 1){ //Transmit UART receive buffer
-    for (int i = pos - 1; i >= pos; i--){
-      if (i < 0){ //P
-        i = 254;
-      }
-      Wire.write(outbuf[i]);
+    if (--pos < 0){
+      pos = 254;
     }
-    pos = 0;
+    Wire.write(outbuf[pos]);
   }else if (mode == 2){ //I2C Delay: First byte delay in 100ms intervals, Second byte command, third byte data
-    if (len == 3){
-      byte t = Wire.read();
-      byte cmd = Wire.read();
-      byte data = Wire.read();
-      delay(t * 100);
-      Wire.beginTransmission(0x2b);
-      Wire.write(cmd);
-      Wire.write(data);
-      Wire.endTransmission();
-    }
+    byte t = Wire.read();
+    delay(t * 100);
+    Wire.beginTransmission(0x2b);
+    Wire.write(0x70);
+    Wire.write(0x0F);
+    Wire.endTransmission();
   }
 }
 
